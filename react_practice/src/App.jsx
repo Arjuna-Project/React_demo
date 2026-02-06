b588f3b54c177183228dcfdde3d0a4c9dc621540
Arjuna-Project <arjunvk1819@gmail.com>
2026-02-06 16:52:43 +0530

    Initial commit

---
+++ react_practice/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Card from './components/Card.jsx'
+import './assets/Flex Image Ratio.png'
+
+function App() {
+
+
+  return (
+    <>
+      <Card/>
+    </>
+  )
+}
+
+export default App
---
+++ react_practice/src/components/Card.jsx
@@ -0,0 +1,34 @@
+import img1 from '../assets/Flex Image Ratio.png'
+import './Card.css'
+
+function Card(){
+    return(
+        <div className="container">
+            <div className="card">
+                <img src={img1} alt="Telephone image"></img>
+            </div>
+            <p className='new'>NEW ARRIVAL</p>
+            <h2>SMARTPHONE XYZ</h2>
+            <div className="badges">
+                <p className='Free'>Free Shipping</p>
+                <p className='Offer'>Limited Offer</p>
+            </div>
+            <div className="divider"></div>
+            <div className="colors">
+                <span className="blue"></span>
+                <span className="green"></span>
+            </div>
+            <div className="price">
+                <h1>999,999</h1>
+            </div>
+            <div className="content">
+                <p>The SmartPhone XYZ combines cutting-edge mobile technology with sleek design and a powerful camera.
+                    Perfect for those seeking performance and style.
+                </p>
+            </div>
+        <button>BUY NOW</button>
+        </div>
+    )
+}
+
+export default Card;
